f44d5b6f5474f24979a10124a06f76987ecf07f6
jackytsheng <jackytsheng@gmail.com>
2020-04-28 00:52:42 +1000

    fix some spacing problem for each section

--- project/personal-website/form.js
+++ project/personal-website/form.js
@@ -6,26 +6,54 @@ const email = document.querySelector("#email");
 const emailIcon = document.querySelector(".email__icon");
 const emailText = document.querySelector(".info__email");
 
-const inputBar = document.querySelectorAll(".input__bar");
+const message = document.querySelector("#contactMessages");
+const messageIcon = document.querySelector(".message__icon");
+const messageText = document.querySelector(".info__message");
+
+
+const inputBar = document.querySelectorAll(".underline__focus");
 FullName.onfocus = ()=>{
   FullNameIcon.style.color = "#3fc1c9";
   FullNameText.style.transform = "translateY(-20px)";
   FullNameText.style.fontSize = "12px";
+  inputBar[0].style.left ="0";
+  inputBar[0].style.right= "0";
 };
 FullName.onblur = ()=>{
   FullNameIcon.style.color = "";
   FullNameText.style.transform = "";
   FullNameText.style.fontSize = "";
+  inputBar[0].style.left = "50%";
+  inputBar[0].style.right = "50%";
 };
 
 email.onfocus = () => {
   emailIcon.style.color = "#3fc1c9";
   emailText.style.transform = "translateY(-20px)";
   emailText.style.fontSize = "12px";
+  inputBar[1].style.left = "0";
+  inputBar[1].style.right = "0";
 };
 email.onblur = () => {
   emailIcon.style.color = "";
   emailText.style.transform = "";
   emailText.style.fontSize = "";
+  inputBar[1].style.left = "50%";
+  inputBar[1].style.right = "50%";
 };
 
+message.onfocus = () => {
+  messageIcon.style.color = "#3fc1c9";
+  messageText.style.transform = "translateY(-23px)";
+  messageText.style.fontSize = "12px";
+  inputBar[2].style.left = "0";
+  inputBar[2].style.right = "0";
+};
+
+message.onblur = () => {
+  messageIcon.style.color = "";
+  messageText.style.transform = "";
+  messageText.style.fontSize = "";
+  inputBar[2].style.left = "";
+  inputBar[2].style.right = "";
+};
