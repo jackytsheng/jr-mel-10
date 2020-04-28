2d2634057595facc6470e3ae1d9fb055cb488e0b
jackytsheng <jackytsheng@gmail.com>
2020-04-28 23:30:57 +1000

    added the animation to the navbar item

--- project/personal-website/nav-control.js
+++ project/personal-website/nav-control.js
@@ -1,10 +1,38 @@
 
 const navBar = document.querySelector(".nav");
-
+const navItem = document.querySelectorAll(".nav__content__link ");
+var currentPage = "#home";
+var previousPage = "";
 
 navStickyDetection = () => {
     window.pageYOffset > 30 ? navBar.classList.add("nav-sticky") : navBar.classList.remove("nav-sticky");
 }
 
 window.onscroll = navStickyDetection;
+
+navItem.forEach(element =>{
+    element.onclick = (evt)=>{
+        evt.preventDefault();
+        var newPage = evt.target.getAttribute("href");
+
+        push(newPage);
+    }
+});
+
+
+function push(to){
+    if (to !== currentPage){
+        previousPage = currentPage;
+       currentPage = to;  
+       
+       changePage();
+    }
+}
+
+function changePage(){
+    var className = "nav__content__link--selected";
+    document.querySelector(`[href='${previousPage}']`).classList.remove(className);
+    var selectedItem = document.querySelector(`[href='${currentPage}']`);
+    selectedItem.classList.add(className);
+}
 
